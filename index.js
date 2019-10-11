15515e52f8e24bffa7f00205ebac8b4dc290adec
Erwin-Cino <ErwinCino0812@gmail.com>
2019-10-11 16:35:46 +0800

    rest api

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const path = require("path");
+const members = require("./Members");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+//set a static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//Gets all members
+app.get("/api/members", (req, res) => res.json(members));
 
 const PORT = process.env.PORT || 5000;
 
